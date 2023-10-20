af388135416dcd0aed409908393e5b9626932f9a
unknown <sclauguico@gmail.com>
2023-10-20 11:52:40 +0800

    ADD: pylint - static code analysis

---
+++ 05-activity-static-code-analysis/sample2.py
@@ -0,0 +1,14 @@
+def add(number1, number2):
+    """
+    This function takes two numbers as input and returns their sum.
+
+    :param number1: The first number
+    :param number2: The second number
+    :return: The sum of the two numbers
+    """
+    return number1 + number2
+
+NUM1 = 4
+NUM2 = 5
+TOTAL = add(NUM1, NUM2)
+print(f"The sum of {NUM1} and {NUM2} is {TOTAL}")
