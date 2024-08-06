e1326ac72e2ed62ab6f50a749bc96124a03eba51
Sandy Lauguico <sclauguico@gmail.com>
2024-08-06 23:15:00 +0800

    ADD: mysql restore and backup exercise

---
+++ 33-mysql-restore-and-backup/world_mysql_update_A.sql
@@ -0,0 +1,19 @@
+SET FOREIGN_KEY_CHECKS=0;
+
+INSERT INTO `country` VALUES ('CAN','Canada','North America','North America',9970610.00,1867,31147000,79.4,598862.00,625626.00,'Canada','Constitutional Monarchy, Federation','Elisabeth II',1822,'CA');
+
+
+INSERT INTO `countrylanguage` VALUES ('CAN','Chinese','F',2.5);
+INSERT INTO `countrylanguage` VALUES ('CAN','Dutch','F',0.5);
+INSERT INTO `countrylanguage` VALUES ('CAN','English','T',60.4);
+INSERT INTO `countrylanguage` VALUES ('CAN','Eskimo Languages','F',0.1);
+INSERT INTO `countrylanguage` VALUES ('CAN','French','T',23.4);
+INSERT INTO `countrylanguage` VALUES ('CAN','German','F',1.6);
+INSERT INTO `countrylanguage` VALUES ('CAN','Italian','F',1.7);
+INSERT INTO `countrylanguage` VALUES ('CAN','Polish','F',0.7);
+INSERT INTO `countrylanguage` VALUES ('CAN','Portuguese','F',0.7);
+INSERT INTO `countrylanguage` VALUES ('CAN','Punjabi','F',0.7);
+INSERT INTO `countrylanguage` VALUES ('CAN','Spanish','F',0.7);
+INSERT INTO `countrylanguage` VALUES ('CAN','Ukrainian','F',0.6);
+
+SET FOREIGN_KEY_CHECKS=1;
